1f87f61613fb1d2c54c43eeb88b45af27271d61a
FelipeLisboa <lisboafelipe38@gmail.com>
2026-05-26 17:08:49 -0300

    Add files via upload

---
+++ ex05.js
@@ -0,0 +1,14 @@
+console.log(" -------- Números -------- ")
+console.log("")
+
+let ns = [3, 3, 5,]
+console.log(n)
+console.log("")
+
+let soma = 0
+for(let i = 0; i < ns.length; i++ ){
+   soma = soma + ns[i] 
+}
+console.log(" -------- Resutado da soma --------")
+console.log("")
+console.log(`A soma deu ${soma}`)
