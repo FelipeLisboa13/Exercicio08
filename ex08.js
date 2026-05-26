7d3ac96e7f9d9481d532ebc0cd15cdab5daac732
FelipeLisboa <lisboafelipe38@gmail.com>
2026-05-26 17:09:51 -0300

    Add files via upload

--- ex08.js
+++ ex08.js
@@ -1,17 +1,9 @@
-// Crie um for que mostre a tabuada de um número, onde o usuário informe qual número será usado, 
-// e o valor inicial e o valor final da tabuada.
-
-const readline = require("readline-sync")
-
-let n = readline.questionFloat("Qual é o número da tabuada: ")
-let n2 = readline.questionFloat("qual é o número que você quer multiplicar: ")
-let n3 = readline.questionFloat("Até onde você quer a tabuada:")
-
-console.log("-------------- Tabuada --------------")
+let ids = [5,3,6,3,34,3,33,3,63,46,36,3,63,64,63,6,3,3,4,6,3,3,6,3,63]
+ids.sort((a,b)=> a-b)
+console.log(" _________________----------________________")
 console.log("")
-console.log(`Especificações do Usuário: Começa no ${n} é multiplicado por ${n2} e acaba no ${n3} `)
+console.log(" --------- idade em ordem crescente --------- ")
 console.log("")
 
-for(let i = n2; i <= n3; i++){ 
-console.log(` ${n} x ${i} = ${n*i}`)
-}
+console.log(`${ ids }`)
+console.log("")
