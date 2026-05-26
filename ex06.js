0ea8f8e69a264de750002727176f2d2905b54620
FelipeLisboa <lisboafelipe38@gmail.com>
2026-05-26 17:09:12 -0300

    Add files via upload

---
+++ ex06.js
@@ -0,0 +1,13 @@
+let id = [5,6,8]
+console.log("")
+console.log(` Quanto vai dar a soma de ${id}`)
+let soma = 0
+
+
+for(let i = 0; i < id.length; i++){
+soma = soma + id[i]
+
+}
+console.log("")
+console.log(`A soma da idade deu ${soma}`)
+console.log("")
